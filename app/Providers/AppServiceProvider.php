494f280d9ec29bc8d4dc22b76f5d12f23f4563da
Nuno Maduro <enunomaduro@gmail.com>
2024-08-20 17:33:46 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Artisan;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register any application services.wqdqwd
      */
     public function register(): void
     {
