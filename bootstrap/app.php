4b1e1b0aa7a3dddb023730a82a3fe7bdac979156
Nuno Maduro <enunomaduro@gmail.com>
2024-08-20 17:37:42 +0100

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
